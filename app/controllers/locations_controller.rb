52155d1f9a90427dd4371c5b4c228ed4ed9b3872
Shvarts <svarts@ukr.net>
2012-11-02 15:44:35 +0200

    add search and modify parse excel

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,11 +1,21 @@
 class LocationsController < ApplicationController
-
+  before_filter :authenticate, :except => [:index, :show]
   respond_to :json, :html
   # GET /locations
   # GET /locations.xml
   def index
-    @locations = Location.all
-    @json = Location.all.to_gmaps4rails
+    @locs = Location.all
+        @search = params[:search]
+        @locations = []
+    @locs.each do |l|
+      if l.address.index(@search.to_s) && @search !="" && @search !=nil
+        @locations<<l
+      end
+    end
+    if @locations.empty?
+      @locations = Location.all
+    end
+    @json = @locations.to_gmaps4rails
     respond_with @json
   end
 
@@ -89,28 +99,59 @@ class LocationsController < ApplicationController
     File.open(Rails.root.join('app', 'assets', 'files', uploaded_io.original_filename), 'wb+') do |file|
       file.write(uploaded_io.read)
     end
+    #file_format = uploaded_io.to_s.split(".")
+    #if file_format[1]=="xls"
     book = Spreadsheet.open("#{Rails.root}/app/assets/files/#{uploaded_io.original_filename}")
     sheet1 = book.worksheet 0
-    i=0
+    row_size = 0
+    col_size = 0
+
     sheet1.each do |row|
-      if row!='' || row!=nil        
-        @location = Location.new(:address => row[0].to_s, :zip => row[1])
-#        @location.process_geocoding
-        i+=1
-        if i>=2
-          @location.save
-        end  
+      row_size = row.size
+      if row[0]!=''
+        col_size +=1
       end
     end
+    col_size+=1
+    addresses = []
+    zips = []
+    a_n = nil
+    z_n = nil
+    row_size.times do |i|
+      sheet1.each do |row|
+        if row!='' || row!=nil
+          if a_n==i
+            addresses<<row[i].to_s
+          elsif z_n==i
+            zips<<row[i].to_i 
+          end
+          if row[i].to_s == 'Address'
+            a_n=i
+          elsif row[i].to_s == 'Zip+4' || row[i].to_s == 'Zip'
+            z_n=i
+          end
+        end
+      end
+    end
+
+    addresses.size.times do |i|
+      @location = Location.new(:address => addresses[i], :zip => zips[i])
+      @location.save
+    end
+
     respond_to do |format|
-      if @location.save
+    #  if @location.save
         format.html { redirect_to(@location, :notice => 'Location was successfully created.') }
         format.xml  { render :xml => @location, :status => :created, :location => @location }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @location.errors, :status => :unprocessable_entity }
-      end
+    #  else
+    #    format.html { render :action => "new" }
+    #    format.xml  { render :xml => @location.errors, :status => :unprocessable_entity }
+     # end
     end  
   #  redirect_to locations_path
   end  
+  def authenticate
+     redirect_to("/users/sign_in") unless user_signed_in?
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 DastConsulting::Application.routes.draw do
+  devise_for :users
+
   resources :locations
   match 'locations/excel' => 'locations#excel'
   # The priority is based upon order of creation:
